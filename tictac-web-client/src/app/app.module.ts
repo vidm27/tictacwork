3195e613a633a11e13cd91ff919dadbfa37b29ba
vidm27 <davidmoran464@gmail.com>
2023-08-26 19:57:05 -0500

    feat[config]: Import componente task refs: #1

--- tictac-web-client/src/app/app.module.ts
+++ tictac-web-client/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimerTaskModule } from './timer/timer.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TimerTaskModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tictac-web-client/src/app/timer/timer.module.ts
+++ tictac-web-client/src/app/timer/timer.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from "@angular/core";
+import { AddTaskTimerComponent } from "./components/add-task-timer/add-task-timer.component";
+import { ListTaskComponent } from "./components/list-task/list-task.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  providers: []
+  declarations: [
+    AddTaskTimerComponent,
+    ListTaskComponent
+  ],
+  exports: [
+    ListTaskComponent, AddTaskTimerComponent
+  ],
+  imports: [
+    CommonModule
+  ],
 })
 export class TimerTaskModule{}
