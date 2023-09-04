2f82b524a83cfb7e18293036e2c080ccdeeeedbb
vidm27 <davidmoran464@gmail.com>
2023-09-04 02:01:09 -0500

    feat: render all new task added to tracking

--- tictac-web-client/src/app/timer/components/list-task/list-task.component.ts
+++ tictac-web-client/src/app/timer/components/list-task/list-task.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TaskTimer } from '../../interfaces/task.interface';
+import { utc, Duration, duration, isDuration } from 'moment';
+
 
 @Component({
   selector: 'app-list-task',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class ListTaskComponent {
 
+  @Input()
+  public tasks: TaskTimer[] = [
+  ]
+
+  public formatTime(moment: Duration|number, allowSecond: boolean = false) {
+    // const newMoment = Math.floor(moment / 1000)
+
+
+
+    if (isDuration(moment)){
+      return utc(moment.milliseconds()).format('HH:mm:ss')
+    }else{
+      moment = duration(moment)
+      return utc(moment.milliseconds()).format('HH:mm:ss')
+    }
+
+    // return÷ utc(moment.milliseconds()).format('HH:mm:ss')
+  }
+  private formatTimeSegment(segment: number): string {
+    return segment < 10 ? `0${segment}` : segment.toString();
+  }
 }
