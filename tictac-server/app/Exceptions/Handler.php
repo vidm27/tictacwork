c947d9fc445a2d0f6a950dbf2281c6ee7cd4f158
vidm27 <davidmoran464@gmail.com>
2023-09-24 01:32:41 -0500

    api exception handler

--- tictac-server/app/Exceptions/Handler.php
+++ tictac-server/app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Routing\Route;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +28,13 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if($exception instanceof ModelNotFoundException && $request -> isJson()){
+            return Route::responseWithRouter('api.fallback.404');
+        }
+        return parent::render($request, $exception);
     }
 }
