09f3e53c3e75c0ce58c5cdcf7265bf8e0027ef8f
vidm27 <davidmoran464@gmail.com>
2023-11-29 20:55:43 -0500

    Delete mobile app

--- tictac_mobile/android/app/src/main/kotlin/com/example/tictac_mobile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tictac_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- tictac_mobile/lib/config/router/app_router.dart
+++
@@ -1,9 +0,0 @@
-import 'package:go_router/go_router.dart';
-import 'package:tictac_mobile/timer/pages/home_page.dart';
-
-final appRouter = GoRouter(routes: [
-  GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) =>  HomeScreen())
-]);
