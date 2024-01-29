61155963730facad978f49621a4eb990f51a98d1
vidm27 <davidmoran464@gmail.com>
2024-01-28 21:04:09 -0500

    modelo de client

--- tictac-server/app/Models/Client.php
+++ tictac-server/app/Models/Client.php
@@ -3,12 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
     use HasUuids;
+    use HasFactory;
+
+    protected $fillable = ['name'];
 
     public function projects(): HasMany{
         return $this->hasMany(Project::class);
